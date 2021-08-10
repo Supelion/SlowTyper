ccf3d53866b8d221d2184fbd9eabafbb189c3da2
Supelion <mutayyabtariq9@gmail.com>
2021-08-10 21:17:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ import time
 import sys
 
 string = input("Enter a string to be written in the typing effect: ")
-speed = float(input("Enter the speed of the typing effect: "))
+
+if len(string) < 1:
+    string = "Hello World, I am a Python Program being typed out in the typewriter effect!"
+
+userInputspeed = input("Enter the speed of the typing effect: ")
+
+if len(userInputspeed) < 1:
+    userInputspeed = 0.05
+
+try:
+    speed = float(userInputspeed)
+except:
+    raise Exception("Please enter a valid number (it can be a decimal number)")
 
 for char in string:
     sys.stdout.write(char)
     sys.stdout.flush()
-    time.sleep(0.05)
+    time.sleep(speed)
